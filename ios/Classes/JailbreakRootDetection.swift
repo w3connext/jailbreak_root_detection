dbcbcae7baca662543c531a438bce2dda91bf327
“prongbang” <prongbang@gmail.com>
2023-01-24 10:05:09 +0700

    Refactor function to class JailbreakRootDetection

---
+++ ios/Classes/JailbreakRootDetection.swift
@@ -0,0 +1,38 @@
+//
+//  JailbreakRootDetection.swift
+//  jailbreak_root_detection
+//
+//  Created by M on 24/1/2566 BE.
+//
+
+import Foundation
+import IOSSecuritySuite
+
+class JailbreakRootDetection {
+    
+    func checkJailBroken() -> Bool {
+        let isJailBroken = UIDevice.current.isJailBroken
+        let amJailbroken = IOSSecuritySuite.amIJailbroken()
+        let amDebugged = IOSSecuritySuite.amIDebugged()
+        let amReverseEngineered = IOSSecuritySuite.amIReverseEngineered()
+        let amProxied = IOSSecuritySuite.amIProxied()
+        let fridaFound = FridaChecker.isFound()
+        let cydiaFound = CydiaChecker.isFound()
+        
+        return isJailBroken
+        || amJailbroken
+        || amDebugged
+        || amReverseEngineered
+        || amProxied
+        || fridaFound
+        || cydiaFound
+    }
+    
+    func checkRealDevice() -> Bool {
+        let isSimulator = UIDevice.current.isSimulator
+        let amEmulator = IOSSecuritySuite.amIRunInEmulator()
+        
+        return !isSimulator && !amEmulator
+    }
+    
+}
--- ios/Classes/SwiftJailbreakRootDetectionPlugin.swift
+++ ios/Classes/SwiftJailbreakRootDetectionPlugin.swift
@@ -4,57 +4,32 @@ import IOSSecuritySuite
 
 public class SwiftJailbreakRootDetectionPlugin: NSObject, FlutterPlugin {
     
+    private var jailbreakRootDetection: JailbreakRootDetection? = nil
+        
+    init(jailbreakRootDetection: JailbreakRootDetection) {
+        self.jailbreakRootDetection = jailbreakRootDetection
+    }
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "jailbreak_root_detection", binaryMessenger: registrar.messenger())
-        let instance = SwiftJailbreakRootDetectionPlugin()
+        
+        let jailbreakRootDetection = JailbreakRootDetection()
+        
+        let instance = SwiftJailbreakRootDetectionPlugin(jailbreakRootDetection: jailbreakRootDetection)
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "isJailBroken":
-            result(checkJailBroken())
+            result(self.jailbreakRootDetection?.checkJailBroken())
             break
         case "isRealDevice":
-            result(checkRealDevice())
+            result(self.jailbreakRootDetection?.checkRealDevice())
             break
         default:
             result(FlutterMethodNotImplemented)
         }
-    }
-    
-    func checkJailBroken() -> Bool {
-        let isJailBroken = UIDevice.current.isJailBroken
-        let amJailbroken = IOSSecuritySuite.amIJailbroken()
-        let amDebugged = IOSSecuritySuite.amIDebugged()
-        let amReverseEngineered = IOSSecuritySuite.amIReverseEngineered()
-        let amProxied = IOSSecuritySuite.amIProxied()
-        let fridaFound = FridaChecker.isFound()
-        let cydiaFound = CydiaChecker.isFound()
-        
-        print("isJailBroken: \(isJailBroken)")
-        print("amJailbroken: \(amJailbroken)")
-        print("amDebugged: \(amDebugged)")
-        print("amReverseEngineered: \(amReverseEngineered)")
-        print("amProxied: \(amProxied)")
-        
-        return isJailBroken
-        || amJailbroken
-        || amDebugged
-        || amReverseEngineered
-        || amProxied
-        || fridaFound
-        || cydiaFound
-    }
-    
-    func checkRealDevice() -> Bool {
-        let isSimulator = UIDevice.current.isSimulator
-        let amEmulator = IOSSecuritySuite.amIRunInEmulator()
-        
-        print("isSimulator: \(isSimulator)")
-        print("amEmulator: \(amEmulator)")
-        
-        return !isSimulator && !amEmulator
     }
     
 }
